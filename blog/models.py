0cb1db18ffd3b8252420c06d03a3964e30b650a1
Muhafandi <garamgudang1917@gmail.com>
2021-07-27 14:10:39 +0800

    asdfasdfa

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=250)
+	body = models.TextField(blank=True)
+	created_at = models.DateTimeField(default=datetime.now,blank=True)	
