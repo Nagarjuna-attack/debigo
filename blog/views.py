0343c3508b979d766b027bb612c83c1004bb78d0
Muhafandi <garamgudang1917@gmail.com>
2021-07-28 02:44:44 +0800

    asdfadsf

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-	return render(request,'index.html')
+	posts = Post.objects.all().order_by('-id')
+	return render(request,'index.html',{'posts':posts})
+
+
+def post(request, pk ):
+	read = Post.objects.get(id=pk)
+	return render(request,'post.html',{'read':read})
